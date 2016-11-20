d7cf382c64ca6f64de971e8687cf98dfd0ccd7b1
romulus <executiveuptown@gmail.com>
2016-11-20 22:06:58 +0000

    updated home pae

--- js/tubgallery.js
+++ js/tubgallery.js
@@ -3,8 +3,8 @@
 /*tubular plugin for video background*/
 
 $(document).ready(function() {
-  $('#wrapper').tubular({videoId: 'ahlywIKP5_c'});
-    /*    $('#wrapper').tubular({videoId: 'Xm4huNBW0Ho'});  */
+ /* $('#wrapper').tubular({videoId: 'ahlywIKP5_c'});  */
+        $('#wrapper').tubular({videoId: 'Td4RHvyAFsM'});  
 });
 
 $('#offer').delay(2000).fadeTo('medium', 0);
